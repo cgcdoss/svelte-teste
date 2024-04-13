80c434b2676dcb983228438b721de3c7db7482d7
cgcdoss <gabi04061997@gmail.com>
2024-04-13 14:39:59 -0300

    feat: ajustando header para mobile

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -17,20 +17,49 @@
     path: m,
     name: m === "" ? "Home" : m.substring(0, 1).toUpperCase() + m.substring(1),
   }));
+
+  let showItems = false;
 </script>
 
 <nav class="bg-primary-950 w-full">
-  <ul class="container mx-auto px-8 py-4 flex gap-4">
-    {#each routes as route}
-      <li>
-        <a
-          href="{base}/{route.path}"
-          class="text-white hover:bg-primary-700 px-4 py-2 transition-all"
-          class:bg-primary-500={currentUrl === route.path}
-        >
-          {route.name}
-        </a>
-      </li>
-    {/each}
-  </ul>
+  <div
+    class="flex container mx-auto px-8 py-4 gap-4"
+    class:flex-col={showItems}
+  >
+    <button class="block md:hidden" on:click={() => (showItems = !showItems)}>
+      <svg
+        class="w-5 h-5"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 17 14"
+      >
+        <path
+          stroke="#fff"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M1 1h15M1 7h15M1 13h15"
+        />
+      </svg>
+    </button>
+    <ul
+      class="hidden md:flex"
+      class:!flex={showItems}
+      class:flex-col={showItems}
+      class:gap-4={showItems}
+    >
+      {#each routes as route}
+        <li>
+          <a
+            href="{base}/{route.path}"
+            class="text-white hover:bg-primary-700 px-4 py-2 transition-all"
+            class:bg-primary-500={currentUrl === route.path}
+          >
+            {route.name}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </div>
 </nav>
