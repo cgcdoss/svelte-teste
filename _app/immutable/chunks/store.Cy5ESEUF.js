37294e0115c8a6e3ef175743ea9c64296d3dcd42
cgcdoss <gabi04061997@gmail.com>
2024-04-13 16:48:04 -0300

    Updates

--- _app/immutable/chunks/store.Cy5ESEUF.js
+++
@@ -1,1 +0,0 @@
-import{w as a}from"./index.Dvw628dj.js";const i=a(!1),o=a(""),e=a(!1);e.set(matchMedia("(max-width: 425px)").matches),matchMedia("(max-width: 425px)").addEventListener("change",t=>e.set(t.matches));export{o as e,e as i,i as l};
---
+++ _app/immutable/nodes/4.LGgUiFCU.js
@@ -0,0 +1,1 @@
+import{P as m}from"../chunks/4.Cuif8hch.js";export{m as component};
