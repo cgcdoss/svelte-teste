869eee9e2fcdf6005fab396f05db5ef8fa2b362e
cgcdoss <gabi04061997@gmail.com>
2024-04-13 15:41:22 -0300

    feat: usando --mode do Vite para diferenciar ambientes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+  const modo = import.meta.env.MODE;
+</script>
+
 <svelte:head>
   <title>Svelte teste</title>
 </svelte:head>
@@ -6,3 +10,10 @@
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+
+<hr class="my-4" />
+{#if modo === "dev"}
+  <p>O ambiente atual é: "dev"</p>
+{:else if modo === "prod"}
+  <p>O ambiente atual é: "prod"</p>
+{/if}
