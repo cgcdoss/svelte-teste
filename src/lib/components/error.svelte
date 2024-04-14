7dab118e52475addf9d002c3f70b2ef9cdc90d89
cgcdoss <gabi04061997@gmail.com>
2024-04-13 21:56:10 -0300

    refactor: criando componentes para erro e loading

---
+++ src/lib/components/error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { error } from "$lib/store";
+</script>
+
+{#if $error}
+  <div class="fixed top-16 right-3 bg-red-200 text-red-900 p-4">
+    {$error}
+    <button class="ml-2" on:click={() => error.set("")}>&times;</button>
+  </div>
+{/if}
