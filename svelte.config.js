1158db35ae9b21de5a6816732f8392b184f32d07
cgcdoss <gabi04061997@gmail.com>
2024-04-16 22:16:01 -0300

    chore: mudando forma de obter base path dinamicamente para o GithubPages
    https://kit.svelte.dev/docs/adapter-static#github-pages

--- svelte.config.js
+++ svelte.config.js
@@ -21,7 +21,8 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/svelte-teste' : '' // Para funcionar no Github Pages
+      // base: process.env.NODE_ENV === 'production' ? '/svelte-teste' : '' // Para funcionar no Github Pages
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH // Para funcionar no Github Pages
     }
   },
 };
