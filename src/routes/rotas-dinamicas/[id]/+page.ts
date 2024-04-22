e6dcccbd12927dac121ad34fefb337b89b5d5e47
cgcdoss <gabi04061997@gmail.com>
2024-04-22 20:38:11 -0300

    chore: usando optional parameters para não ter que fazer glitch com a função entries()

--- src/routes/rotas-dinamicas/[id]/+page.ts
+++
@@ -1,8 +0,0 @@
-
-// Fazendo glitch para evitar erro ao buildar SSG
-// Dessa forma o builder vai entender que existe apenas um possível id e finalizará com sucesso
-export function entries() {
-  return [
-    { id: '1' },
-  ]
-}
