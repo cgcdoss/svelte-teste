e9227af707175b1a6491ebccc0797d4db2d8fad4
cgcdoss <gabi04061997@gmail.com>
2024-04-22 23:05:52 -0300

    feat: adicionando input type color para usar cor dinâmica

--- src/routes/colors/+layout.svelte
+++ src/routes/colors/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   const colors = ["ff3e00", "676779", "42b4ff", "invalid"];
+  let customColor = "#ff0000";
 </script>
 
 <div class="flex flex-col md:flex-row gap-4 mb-6">
@@ -8,6 +9,17 @@
       #{color}
     </a>
   {/each}
+
+  <div class="flex gap-1">
+    <input type="color" bind:value={customColor} />
+    <a
+      class="p-2"
+      href={"/colors/pick/" + customColor.replace("#", "")}
+      style="--color: {customColor}"
+    >
+      {customColor}
+    </a>
+  </div>
 </div>
 
 <slot />
