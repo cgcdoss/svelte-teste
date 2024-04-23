c85ba25fa16280b56a375e2feed1e5c65b68e047
cgcdoss <gabi04061997@gmail.com>
2024-04-22 22:35:32 -0300

    feat: usando param matchers

---
+++ src/routes/colors/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  const colors = ["ff3e00", "676779", "42b4ff", "invalid"];
+</script>
+
+<div class="flex flex-col md:flex-row gap-4 mb-6">
+  {#each colors as color}
+    <a class="p-2" href={"/colors/pick/" + color} style="--color: #{color}">
+      #{color}
+    </a>
+  {/each}
+</div>
+
+<slot />
+
+<style>
+  a {
+    background-color: var(--color);
+  }
+</style>
---
+++ src/routes/colors/pick/[[color=hex]]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  let color = $page.params.color;
+</script>
+
+<h1 class="text-3xl p-4" style="--color: #{$page.params.color}">
+  Color is: {color}
+</h1>
+
+<style>
+  h1 {
+    background-color: var(--color);
+  }
+</style>
