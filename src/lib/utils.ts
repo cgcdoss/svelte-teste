fc250c0c2391e100195d9a574dcfd51a664b8156
cgcdoss <gabi04061997@gmail.com>
2024-04-23 20:13:15 -0300

    feat: usando função get()

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,14 @@
+import { get } from "svelte/store";
+import { isMobile } from "./store";
 
 export function getCookie(name: string) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2)
     return parts.pop()?.split(';').shift();
+}
+
+// Função get() serve para obter valor de stores quando estamos fora do contexto de um componente
+export function mobile() {
+  return get(isMobile);
 }
