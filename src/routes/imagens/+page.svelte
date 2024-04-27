49bc5c5c1830dd32d757fa739560d569bab87d56
cgcdoss <gabi04061997@gmail.com>
2024-04-27 00:01:32 -0300

    chore: correção para efeito slide voltar a funcionar na página de imagens

--- src/routes/imagens/+page.svelte
+++ src/routes/imagens/+page.svelte
@@ -6,38 +6,45 @@
   import { fly } from "svelte/transition";
 
   let cor = "#FF0000";
+  let exibir = false;
+
+  setTimeout(() => {
+    exibir = true;
+  }, 200);
 </script>
 
 <svelte:head>
   <title>Svelte teste - Imagens</title>
 </svelte:head>
 
-<h1>Imagem da internet</h1>
-<img
-  src="https://thispersondoesnotexist.com/"
-  alt="pessoa não existe"
-  class="h-[200px]"
-  in:fly={{ x: 100, delay: 250 }}
-/>
+{#if exibir}
+  <h1>Imagem da internet</h1>
+  <img
+    src="https://thispersondoesnotexist.com/"
+    alt="pessoa não existe"
+    class="h-[200px]"
+    in:fly={{ x: 100, delay: 250 }}
+  />
 
-<h1>Imagem da pasta static</h1>
-<img
-  src={base + "/imgs/doguinho.avif"}
-  alt="doguinho"
-  class="h-[200px] max-w-full object-cover"
-  in:fly={{ x: 100, delay: 600 }}
-/>
+  <h1>Imagem da pasta static</h1>
+  <img
+    src={base + "/imgs/doguinho.avif"}
+    alt="doguinho"
+    class="h-[200px] max-w-full object-cover"
+    in:fly={{ x: 100, delay: 600 }}
+  />
 
-<h1>Imagem da pasta lib</h1>
-<img src={imageUrso} alt="urso" class="h-[200px]" />
+  <h1>Imagem da pasta lib</h1>
+  <img src={imageUrso} alt="urso" class="h-[200px]" />
 
-<h1>Imagem dentro de componente (svg)</h1>
-<IconHome color={cor} class="img" />
-<label for="cor">Cor do ícone:</label>
-<input type="color" id="cor" bind:value={cor} />
+  <h1>Imagem dentro de componente (svg)</h1>
+  <IconHome color={cor} class="img" />
+  <label for="cor">Cor do ícone:</label>
+  <input type="color" id="cor" bind:value={cor} />
 
-<h1>Imagem dentro de componente (png)</h1>
-<SvelteLogo />
+  <h1>Imagem dentro de componente (png)</h1>
+  <SvelteLogo />
+{/if}
 
 <style>
   h1 {
